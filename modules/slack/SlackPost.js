19151cdab2ff548b326b6c7bbd7bb849f874380c
Jon Falcone <jonfalcone777@gmail.com>
2016-03-13 00:26:56 -0800

    Notification code works. Just need to make it so that provider's webhook is added.

--- modules/slack/SlackPost.js
+++ modules/slack/SlackPost.js
@@ -1,7 +1,6 @@
-/**
- * Created by Jon on 2/27/2016.
- */
 var request = require("request")
+var webHook = "https://hooks.slack.com/services/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"
+//we will need to write a function to return the provider's webhook url
 
 //will post to the default channel, aka "test"
 var data = {
@@ -10,17 +9,16 @@ var data = {
     "icon_emoji": ":clock2:"
 };
 
+//TODO each provider will have their own slack webhook url
 request({
-    //url for the fourtify slack team
-    url: "https://hooks.slack.com/services/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",
+    url: webHook,
     method: "POST",
     json: true,
     headers: {
         "content-type": "application/json"
     },
     body: data
-}, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
+}, function (error, response, body) {    if (!error && response.statusCode === 200) {
         console.log(body)
     }
     else {
