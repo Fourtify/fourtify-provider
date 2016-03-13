45bea4518cbb7654b570e8bccb0bfaf82d50696b
carlngan <carl@salonfrontdesk.com>
2016-03-12 17:11:41 -0800

    updated dynamic names and company name

--- modules/engine/routes/routes.js
+++ modules/engine/routes/routes.js
@@ -6,7 +6,10 @@ var AuthMiddleware = require('../../authentication/src/AuthMiddleware');
 // Menu - GET ============================================================
 // =========================================================================
 router.get('/', AuthMiddleware.authenticate(), function(req, res) {
-    res.render('layout');
+    res.render('layout', {
+        provider: req.session.provider,
+        employee: req.session.employee
+    });
 });
 
 module.exports = router;
