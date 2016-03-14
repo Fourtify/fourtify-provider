54541d17ac062b9afe58ba43e29480269a0e8583
Jon Falcone <jonfalcone777@gmail.com>
2016-03-13 17:59:07 -0700

    Trying to figure out how to grab the webhook url. Code isn't done yet.

--- modules/slack/SlackPost.js
+++ modules/slack/SlackPost.js
@@ -1,11 +1,57 @@
-var request = require("request")
-var webHook = "https://hooks.slack.com/services/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"
+var request = require("request");
+var environment = process.env.NODE_ENV;
+var config = require('../../config/config.json')[environment];
+var API_URL = config.apiUrl;
+
+//angular.module("slack", ["settings"])
+//    .controller("SlackController", ["$scope", "SettingsService",
+//        function($scope){
+//            SettingsService.getSettings(
+//                {},
+//                //success function
+//                function(data) {
+//                    if(data._slack){
+//                        webHook = data._slack;
+//                    }
+//                },
+//                //error function
+//                function(data, status) {
+//                    $scope.clearMessages();
+//                    $scope.err = data;
+//                }
+//            )
+//        }
+//    ])
+
+function getSettings() {
+    var req = {
+        method: 'GET',
+        url: API_URL + '/settings',
+    };
+    request(
+        req,
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body)
+            }
+            else
+            {
+                console.log(body)
+            }
+        }
+    )
+};
+
+var webHook = getSettings();
+//"https://hooks.slack.com/services/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"
+
+
 //we will need to write a function to return the provider's webhook url
 
 //will post to the default channel, aka "test"
 var data = {
     "username": "fourtify-bot",
-    "text": "Hello world!",
+    "text": "A client has arrived for an appointment.",
     "icon_emoji": ":clock2:"
 };
 
@@ -24,7 +70,7 @@ request({
     else {
 
         console.log("error: " + error)
-        console.log("response.statusCode: " + response.statusCode)
-        console.log("response.statusText: " + response.statusText)
+        //console.log("response.statusCode: " + response.statusCode)
+        //console.log("response.statusText: " + response.statusText)
     }
-})
+});
